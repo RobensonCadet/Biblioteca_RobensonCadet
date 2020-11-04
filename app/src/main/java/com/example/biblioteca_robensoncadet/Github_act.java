827b061966903d12366d3dbf05e6b6bbf890ffe0
RobensonCadet <robensoncadet@gmail.com>
2020-11-03 22:33:20 -0300

    Initial commit

---
+++ app/src/main/java/com/example/biblioteca_robensoncadet/Github_act.java
@@ -0,0 +1,58 @@
+package com.example.biblioteca_robensoncadet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+public class Github_act extends AppCompatActivity {
+
+    private Spinner spin;
+    private TextView text;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_github_act);
+
+        spin = (Spinner) findViewById(R.id.spinLibros);
+        text = (TextView) findViewById(R.id.tv);
+
+        String[] Libros = {"Farenheit", "Revival", "El Alquimista"};
+
+        ArrayAdapter<String> adapt = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, Libros);
+        spin.setAdapter(adapt);
+
+    }
+    public void text(View v) {
+
+        String libro = spin.getSelectedItem().toString();
+
+        if (libro.equals("Farenheit")) {
+            int operacion = 5000;
+            String valor = String.valueOf(operacion);
+            text.setText("El valor de " + libro + " es: " + valor);
+            return;
+
+        }
+
+        if (libro.equals("Revival")) {
+            int operacion = 12000;
+            String valor = String.valueOf(operacion);
+            text.setText("El valor de " + libro + " es: " + valor);
+            return;
+
+        }
+
+        if (libro.equals("El Alquimista")) {
+            int operacion = 45000;
+            String valor = String.valueOf(operacion);
+            text.setText("El valor de " + libro + " es: " + valor);
+            return;
+
+        }
+    }
+}
